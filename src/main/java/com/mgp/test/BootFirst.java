6d848ac0b0c31a393fd089fc524a7642732e8c73
3449708385 <3449708385@qq.com>
2018-12-28 15:13:30 +0800

    one

--- src/main/java/com/mgp/test/BootFirst.java
+++ src/main/java/com/mgp/test/BootFirst.java
@@ -5,6 +5,8 @@ public class BootFirst {
     public void getData(int a, int c){
         System.out.println(a);
         System.out.println(c);
+        System.out.println('c');
+        System.err.println("c++");
     }
 
 }
