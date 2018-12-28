8e44543c053936300a0c8ffdf48cb38b06637602
3449708385 <3449708385@qq.com>
2018-12-28 15:10:33 +0800

    one

---
+++ src/main/java/com/mgp/test/BootFirst.java
@@ -0,0 +1,10 @@
+package com.mgp.test;
+
+public class BootFirst {
+
+    public void getData(int a, int c){
+        System.out.println(a);
+        System.out.println(c);
+    }
+
+}
---
+++ src/main/java/com/mgp/test/BootTow.java
@@ -0,0 +1,13 @@
+package com.mgp.test;
+
+public class BootTow {
+
+    // TODO: 2018/12/28
+    public static void main(String[] args) {
+        new BootFirst().getData(10, 12);
+    }
+
+    public void m() {
+
+    }
+}
